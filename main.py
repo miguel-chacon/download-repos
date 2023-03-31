b57b8864e53508043fe5eb7b4be203fe0589c03a
Miguel Chacón <miguel.chacon@idoven.ai>
2023-03-31 18:05:19 +0200

    wip

--- main.py
+++ main.py
@@ -13,7 +13,8 @@ def cli(token, org):
     headers = {'Accept': 'application/vnd.github+json',
                'X-GitHub-Api-Version': '2022-11-28',
                'Authorization': f"Bearer {token}"}
-    r = requests.get(f"https://api.github.com/orgs/{org}/repos", headers=headers)
+    items_per_page = 500
+    r = requests.get(f"https://api.github.com/orgs/{org}/repos?per_page={items_per_page}", headers=headers)
 
     if not os.path.exists(f"{os.getcwd()}/code"):
         os.mkdir(f"{os.getcwd()}/code")
@@ -22,16 +23,20 @@ def cli(token, org):
 
     with tempfile.TemporaryDirectory() as temp_dir:
         for repo in r.json():
-            print(f"Downloading {repo['name']}")
-            res = requests.get(f"https://api.github.com/repos/{org}/{repo['name']}/zipball/main", headers=headers, stream=True)
-            if res.status_code == 404:
-                print(f"Access denied for {repo['name']}")
-            else:
-                with open(f"{temp_dir}/{repo['name']}.zip", 'wb') as fd:
-                    for chunk in res.iter_content(chunk_size=128):
-                        fd.write(chunk)
-                with zipfile.ZipFile(f"{temp_dir}/{repo['name']}.zip") as zip_ref:
-                    zip_ref.extractall(f"{os.getcwd()}/code/{repo['name']}")
+            for branch in ['production', 'main', 'master']:
+                url = f"https://api.github.com/repos/{org}/{repo['name']}/zipball/{branch}"
+                res = requests.get(url, headers=headers, stream=True)
+                if res.status_code == 404:
+                    continue
+                else:
+                    print(f"Downloaded -> repo: {repo['name']} - {branch}")
+                    with open(f"{temp_dir}/{repo['name']}.zip", 'wb') as fd:
+                        for chunk in res.iter_content(chunk_size=1024*1204*10):
+                            fd.write(chunk)
+                    with zipfile.ZipFile(f"{temp_dir}/{repo['name']}.zip") as zip_ref:
+                        zip_ref.extractall(f"{os.getcwd()}/code/{repo['name']}")
+                    break
+                print(f"Error downloading {repo['name']}")
 
 
 if __name__ == '__main__':
